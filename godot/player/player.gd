e38e100cb177c7e5f25e99f93989060f6c73332a
Markus <m.schiller@ymail.com>
2026-04-19 23:32:41 +0200

    trying

--- godot/player/player.gd
+++ godot/player/player.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef6be742ea5bb016127407a7ae2bd0875086828db2fc619ab2241b160012bd4e
-size 3932
+oid sha256:5c51b6e78dc15a435b3c22d255817862c549dfa2e59253ca8402bc1b9aab187a
+size 3933
--- godot/player/statemanager/states/idle.gd
+++ godot/player/statemanager/states/idle.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0443ad2ad79cf5b0053514bd0c27c69552fdffbcc5f3f07e502d891f572061b
-size 1266
+oid sha256:ad089a69fd5cf0dded28581f71f73df6d1a945dd2bc9ea9dcd7e4a524b0b7a56
+size 1375

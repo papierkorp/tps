a1b5eaa514233acb362d1cecbf61a44a30e2dbff
Markus Schiller <m.schiller@ymail.com>
2026-04-26 18:44:49 +0200

    fix: falling state

--- godot/player/statemanager/states/falling.gd
+++ godot/player/statemanager/states/falling.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6539bf6218add12af75b7e3d38505d44e575c9decf0aef5a35cea85873f0a6a
-size 635
+oid sha256:8bbdf1d63d35ecd027b26d6eef05c54ab43816caeef9b84299765bd166389636
+size 739
